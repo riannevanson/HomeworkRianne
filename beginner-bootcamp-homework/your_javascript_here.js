e82cb7e7a442ef97b8eeda0f184d3a3eb675d09f
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-02 13:32:31 +0200

    deel twee voor een deel en begin deel 3

--- beginner-bootcamp-homework/your_javascript_here.js
+++ beginner-bootcamp-homework/your_javascript_here.js
@@ -2,7 +2,7 @@
 var hero = {
   name : 'Sijmen',
   heroic : true,
-  inventory : ['weapons','superpower','food'],
+  inventory : ['weapons','superpower','food','love'],
   health : 8,
     weapon : {
       type: 'string',
@@ -19,8 +19,6 @@ function rest(creature){
 
 // console.log(rest(hero))
 
-
-
 function pickUpItem(creature, item){
 creature.inventory.push(item)
 return creature
@@ -28,18 +26,64 @@ return creature
 // console.log(pickUpItem(hero, "knife"))
 
 
-function dealDamage(){
+function dealDamage(attacker, defender){
 
+defender.health = defender.health - attacker.weapon.damage
+return defender
 }
 
+// console.log(dealDamage(hero,hero))
 
-function equipWeapon(){
 
+function equipWeapon(creature, index){
+// creature.weapon = creature.inventory[index]
+// console.log(creature.inventory[index])
+// creature.inventory = creature.inventory.splice([index], 1)
+// console.log(creature.inventory.splice([index], 1))
+// creature.inventory = creature.inventory - creature.inventory[index]
+// return creature
+}
+// console.log(equipWeapon(hero, 2))
+
+
+function doBattle(heroicCreature, creature){
+
+  if (heroicCreature !== heroic) {
+    return null
+  }
+  //
+  // else if (heroicCreature > 0 && creature > 0){
+  //   dealDamage(heroicCreature, creature)
+  //   if (creature !== 0){
+  //     dealDamage(creature, heroicCreature)
+  //   }
+
+
+  // }
+
+
+
+
+
+
+
+
+
+  // if (heroicCreature !== heroic) {
+  //   return null
+  // }
+  // else {
+  //
+  //   if (heroicCreature > 0 && creature > 0){
+  // //`heroicCreature` deals damage to `creature` first. If `creature` survives it deals damage to `heroicCreature`.
+  //     dealDamage(heroicCreature, creature)
+  //      if (creature !== 0){
+  //       dealDamage(creature, heroicCreature)
+  //     }
+  //   }
+  // }
 }
 
-
-function doBattle(){
-
-}
+console.log(doBattle(hero, hero))
 
 // UI
