0900c9c50eaf06d3db03db693028867590bf83af
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-02 11:04:56 +0200

    yes first one down

--- beginner-bootcamp-homework/your_javascript_here.js
+++ beginner-bootcamp-homework/your_javascript_here.js
@@ -1,5 +1,39 @@
 // Variables
+var hero = {
+  name : 'Sijmen',
+  heroic : true,
+  inventory : ['weapons','superpower','food'],
+  health : 8,
+    weapon : {
+      type: 'string',
+      damage: 5,
+    },
+}
 
 // Game logic
+
+function rest(creature){
+  hero.health
+}
+
+
+function pickUpItem(){
+
+}
+
+
+function dealDamage(){
+
+}
+
+
+function equipWeapon(){
+
+}
+
+
+function doBattle(){
+
+}
 
 // UI
