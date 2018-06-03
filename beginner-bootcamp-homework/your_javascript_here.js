fafc78d7ebc343886ce44f2e9d52ed592e42fbb6
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-03 18:12:52 +0200

    deel drie voor deel

--- beginner-bootcamp-homework/your_javascript_here.js
+++ beginner-bootcamp-homework/your_javascript_here.js
@@ -21,39 +21,22 @@ var badguy = {
     },
 }
 
-// Game logic
-var restEvent = document.getElementById('buttonRest');
-restEvent.onclick = function(restEvent) {
-
-  function rest(creature){
-    creature.health = 10
-    return creature
-  }
-    console.log(rest(hero));
-}
-
-// console.log(rest(hero))
-
-var pickUpEvent = document.getElementById('buttonWeapon');
-pickUpEvent.onclick = function(pickUpEvent) {
-
-  function pickUpItem(creature, item){
-  creature.inventory.push(item)
+var heroStats = hero
+// **********************-------functions-----------****************
+function rest(creature){
+  creature.health = 10
   return creature
-  }
-  console.log(pickUpItem(hero, {type: "gun", damage: 2}))
 }
 
-
+function pickUpItem(creature, item){
+creature.inventory.push(item)
+return creature
+}
 
 function dealDamage(attacker, defender){
-
-defender.health = defender.health - attacker.weapon.damage
-return defender
+  defender.health = defender.health - attacker.weapon.damage
+  return defender
 }
-
-// console.log(dealDamage(hero,hero))
-
 
 function equipWeapon(creature, index){
 // creature.inventory[index]
@@ -61,52 +44,118 @@ creature.weapon = creature.inventory[index]
 creature.inventory.splice(index, 1)
 return creature
 }
-// console.log(equipWeapon(hero, 4))
+
+function doBattle(heroicCreature, creature){
+  if (heroicCreature !== "heroic") {
+    return null
+  }
+  // if (heroicCreature.health > 0 && creature.health > 0){
+  // dealDamage(heroicCreature, creature)
+  //
+  //   if(heroicCreature.health > 0 && creature.health > 0){
+  //     doBattle(heroicCreature, creature)
+  //   } else if(creature.health > 0){
+  //   heroicCreature = 'heroic'
+  //   return heroicCreature
+  // console.log(heroicCreature)
+  //   } else if(heroicCreature.health > 0){
+  //   hero = 'heroic'
+  //   return hero
+  //   console.log(hero)
+  //   }
+  //
+  // } else if(creature.health > 0){
+  // heroicCreature = 'heroic'
+  // doBattle(heroicCreature, creature)
+  // } else if(heroicCreature.health > 0){
+  // hero = 'heroic'
+  // doBattle(heroicCreature, creature)
+  // }
+}
+
+//////////////////////////////////////////////////////////
+
+// *************-----button events:------****************//
+
+
+var restEvent = document.getElementById('buttonRest');
+restEvent.onclick = function(event) {
+
+    function rest(creature){
+      creature.health = 10
+      return creature
+    }
+  console.log(rest(hero));
+}
+
+// console.log(rest(hero))
+
+var pickUpEvent = document.getElementById('buttonWeapon');
+pickUpEvent.onclick = function(event) {
+
+    function pickUpItem(creature, item){
+    creature.inventory.push(item)
+    return creature
+    }
+  console.log(pickUpItem(hero, {type: "gun", damage: 2}))
+}
 
 var battleEvent = document.getElementById('buttonEnemy');
-battleEvent.onclick = function(battleEvent) {
+battleEvent.onclick = function(event) {
 
   function doBattle(heroicCreature, creature){
     if (heroicCreature !== "heroic") {
+      console.log('heroicCreature must play!')
       return null
     }
-    // if (heroicCreature.health > 0 && creature.health > 0){
-    // dealDamage(heroicCreature, creature)
-    //
-    //   if(heroicCreature.health > 0 && creature.health > 0){
-    //     doBattle(heroicCreature, creature)
-    //   } else if(creature.health > 0){
-    //   heroicCreature = 'heroic'
-    //   return heroicCreature
-    // console.log(heroicCreature)
-    //   } else if(heroicCreature.health > 0){
-    //   hero = 'heroic'
-    //   return hero
-    //   console.log(hero)
-    //   }
-    //
-    // } else if(creature.health > 0){
-    // heroicCreature = 'heroic'
-    // doBattle(heroicCreature, creature)
-    // } else if(heroicCreature.health > 0){
-    // hero = 'heroic'
-    // doBattle(heroicCreature, creature)
-    // }
-  }
+
+    if (heroicCreature.health > 0 && creature.health > 0){
+    dealDamage(heroicCreature, creature)
+
+      if(heroicCreature.health > 0 && creature.health > 0){
+        doBattle(heroicCreature, creature)
+      } else if(creature.health > 0){
+
+      return heroicCreature
+    console.log(heroicCreature)
+      } else if(heroicCreature.health > 0){
+
+      return hero
+      console.log(hero)
+      }
+
+    } else if(creature.health > 0){
+    heroicCreature = 'heroic'
+    doBattle(heroicCreature, creature)
+    } else if(heroicCreature.health > 0){
+    hero = 'heroic'
+    doBattle(heroicCreature, creature)
+    }
+
 }
-// console.log(doBattle(badguy,hero))
+console.log(doBattle(badguy,hero))
+
+var AddEquipWeapon = document.getElementById('equipWeapon');
+AddEquipWeapon.addEventListener('click', function(event) {
+    // console.log('Event listener 1');
+  var indexInput = prompt("What weapon number do you want?");
+  // console.log(hero.inventory[indexInput])
+  // console.log(indexInput)
+  console.log(equipWeapon(hero, indexInput))
+});
 
 
+function displayStats(creature){
+var heros = creature;
+document.getElementById("statsBlock").innerHTML = `name: ${heros.name} <br /> health: ${heros.health} <br /> weapontype: ${heros.weapon.type} <br /> weapondamage: ${heros.weapon.damage}`;
+}
+// console.log(displayInventory(hero))
+
+function displayInventory(creature){
+var heros = creature;
+document.getElementById("inventoryBlock").innerHTML = `Inventory: ${heros.inventory} <br /> ${heros.inventory.type} <br /> ${heros.inventory.damage}`;
+}
 
 
-
-
-
-
-
-
-
-
-console.log(doBattle(hero, hero))
 
 // UI
