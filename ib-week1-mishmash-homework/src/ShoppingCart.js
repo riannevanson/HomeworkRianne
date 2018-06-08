747e6a677d4402e3d0107be7e6630ab3241c331f
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-08 20:56:34 +0200

    made class Card

---
+++ ib-week1-mishmash-homework/src/ShoppingCart.js
@@ -0,0 +1,48 @@
+class Cart {
+  constructor(cart) {
+    this.cart = cart;
+  }
+
+  getItems() {
+    let emptyArray = []
+    return emptyArray;
+  }
+  addItem() {
+
+
+    this.getItem.push(getItem)
+    // itemName
+    // quantity
+    // price
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports =  Cart
