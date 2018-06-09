b0d8d96ef851c1c2dd22cd22e5ce178b3d727837
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-09 12:52:42 +0200

    filter aan de praat gekregen

--- ib-week1-mishmash-homework/src/pathFind.js
+++ ib-week1-mishmash-homework/src/pathFind.js
@@ -27,24 +27,43 @@
 //
 //
 
-const pathTest = ["foo", "name", "author"];
-
-const objectTest = {
-  foo: "bar",
-  book: {
-    author: { name: "Dave" },
-    name: "Yuval"
-  }
-};
+// const pathTest = ["foo", "name", "author"];
+//
+// const objectTest = {
+//   foo: "bar",
+//   book: {
+//     author: { name: "Dave" },
+//     name: "Yuval"
+//   }
+// };
 
 function pathFind(path, object){
 
 return object[path]
+}
 
+
+function pathFindMore(path, object){
+  // if (path === object[path])
+  // return "Yuval"//hier moet 'einde van path value komen'
+const getPathItems = path
+  // .map(pathItem => pathItem)
+  .filter(pathItem => pathItem.length > 5)
+console.log(getPathItems)
 
 }
-console.log(pathFind(['foo'], { foo: "bar" }))
 
+
+// console.log(pathFindMore(['foo'], { foo: "bar" }))
+// console.log(pathFindMore(["name"], { name: "Dave" }))
+pathFindMore(["author", "name"], { author: { name: "Stephen" } }) // === "Stephen"
+// console.log(pathFindMore(["book", "author", "name"], {
+//       book: {
+//         author: {
+//           name: "Yuval"
+//         }
+//       }
+//     }))
 
 Output :
 //     pathFind(["foo"], { foo: "bar" }) // === "bar"
