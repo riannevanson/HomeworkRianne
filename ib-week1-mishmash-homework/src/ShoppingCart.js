a1e2eca45bf2931b155f5fe87367f40e8ad55d65
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-10 17:21:39 +0200

    clone works for me, not for the test

--- ib-week1-mishmash-homework/src/ShoppingCart.js
+++ ib-week1-mishmash-homework/src/ShoppingCart.js
@@ -1,3 +1,4 @@
+module.exports =
 class ShoppingCart {
   constructor() {
     this.items = [];
@@ -19,60 +20,23 @@ class ShoppingCart {
   clear() {
     this.items = this.items.filter(item => item !== item);
   }
-  clone(pShoppingCart) {
-
-      Object.create(pShoppingCart).map(x => x);
-      return cloneCart;
-
-  }
+  clone() {
+    let cartClone = new ShoppingCart();
+    cartClone.items = this.items
+    return cartClone
+ }
 }
 
-const cart = new ShoppingCart();
-cart.addItem("yu", 3, 2 + "addItems");
-// console.log(cart.getItems());
+// const cart = new ShoppingCart();
+// cart.addItem("yu", 3, 2 + "addItems");
+// // console.log(cart.getItems());
 // console.log(cart);
-cart.clone(cart);
-const cloneCart = new ShoppingCart();
-cloneCart.clear();
-//console.log(cart);
-//console.log(cloneCart);
-cloneCart.addItem("yu", 3, 2 + "addItems");
-cart.clear();
-console.log(cart);
-console.log(cloneCart);
-
-
-
-
-
-
-
-
-module.exports = class ShoppingCart {
-  constructor() {
-    this.items = [];
-  }
-
-  getItems() {
-    return this.items;
-  }
-
-  addItem(pItemName, pQuantity, pPrice) {
-    var newItem = {
-      name: pItemName,
-      quantity: pQuantity,
-      pricePerUnit: pPrice
-    };
-    this.items.push(newItem);
-  }
-
-  clear() {
-    this.items = this.items.filter(item => item !== item);
-  }
-  clone(pShoppingCart) {
-
-      Object.create(pShoppingCart).map(x => x);
-      return cloneCart;
-
-  }
-}
+// let cloneCart = cart.clone();
+// console.log(cloneCart)
+// // cloneCart.clear();
+// //console.log(cart);
+// //console.log(cloneCart);
+// // cloneCart.addItem("yu", 3, 2 + "addItems");
+// cart.clear();
+// console.log(cart);
+// console.log(cloneCart);
